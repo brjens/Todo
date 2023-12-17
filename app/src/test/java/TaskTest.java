a77f292f2870306b9872e21405ddbbc6de050d00
yoga <brjensen121@gmail.com>
2023-12-16 22:31:35 -0500

    added a few junit tests

--- app/src/test/java/TaskTest.java
+++ app/src/test/java/TaskTest.java
@@ -5,6 +5,8 @@ import android.util.Log;
 
 import com.example.todo.Task;
 
+import java.util.ArrayList;
+
 public class TaskTest {
     @Test
     public void testSetCompleted(){
@@ -28,4 +30,42 @@ public class TaskTest {
 
     }
 
+   @Test
+   public void testArrayList(){
+
+      ArrayList<Task> tasks = getTasksWithRandomNames();
+      String[] names = getRandomNames();
+      String currentName;
+       for (int i=0; i<5; i++){
+           currentName = (tasks.get(i)).getName();
+           assertEquals( currentName, names[i]);
+       }
+   }
+
+   @Test
+   public  void testArrayListChangeName(){
+
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks = getTasksWithRandomNames();
+        String test = "Hello world";
+       //should change Charlie to hello world
+        tasks.get(3).setName(test);
+        assertEquals( tasks.get(3).getName(), "Hello world");
+   }
+   public String[] getRandomNames(){
+        String[] names = {"Alie","bob","Charlie","David","Eva"};
+        return names;
+   }
+
+   //helper, to populate Arraylist w/ random names
+   public  ArrayList<Task> getTasksWithRandomNames(){
+       ArrayList<Task> tasks = new ArrayList<>();
+       Task task;
+       String[] names = getRandomNames();
+       for (int i=0; i<5; i++){
+            task = new Task(names[i]);
+           tasks.add(task);
+       }
+       return tasks;
+   }
 }
